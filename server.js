457ed7c77ad5354a72ff38be2e3799e9605aefcc
Andrew Kwan <akwan84@uwo.ca>
2024-05-03 01:31:46 -0400

    created simple logger

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const { logger } = require('./middleware/logger');
 const app = express();
 
 const PORT = process.env.PORT || 3500;
+
+app.use(logger);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
