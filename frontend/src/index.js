f96bd18283d70a4793eead6f6c4c380083c3c338
Andrew Kwan <akwan84@uwo.ca>
2024-12-17 23:33:11 -0500

    reorganized css

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './css/app.css';
+import './css/login.css';
+import './css/header.css';
+import './css/addWorkout.css';
+import './css/exercise.css';
+import './css/history.css';
 import App from './App';
 import { PageDataProvider } from './context/PageContext';
 import { DataProvider } from './context/DataContext';
